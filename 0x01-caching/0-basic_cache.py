381cc06bab4614a0e8bd920e141c6d2b2e01dc97
Magdy156 <magdyhamdy156@gmail.com>
2024-07-24 21:07:20 +0300

    task0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" 0- basic
+"""
+
+
+from BaseCaching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """basic cashing system"""
+
+    def put(self, key, item):
+        """Assign the value to the key"""
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Return the key value"""
+        return self.cache_data.get(key)
